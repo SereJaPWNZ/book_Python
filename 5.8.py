900ab6588e1eb9a774247913cf5330457722a63a
serejapwnz <oofatherxomgoo@gmail.com>
2023-09-28 21:38:11 +0300

    update 5.8

--- 5.8.py
+++ 5.8.py
@@ -3,3 +3,8 @@
 # • В остальных случаях выводите универсальное приветствие — например, «Hello Jaden, thank you for logging in again».
 
 users = ['John', 'Jane', 'Admin', 'Mike', 'Max']
+for user in users:
+    if user.lower() == 'admin':
+        print('Hello admin, would you like to see a status report?')
+    else:
+        print(f'Hello {user}, thank you for logging in again')
