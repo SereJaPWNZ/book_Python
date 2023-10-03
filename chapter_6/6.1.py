ff2cb57da8d36eb6440c519cd51ad16e1c93c7c5
serejapwnz <oofatherxomgoo@gmail.com>
2023-10-03 21:20:11 +0300

    update 6.1

--- chapter_6/6.1.py
+++ chapter_6/6.1.py
@@ -2,3 +2,11 @@
 # Сохраните имя, фамилию, возраст и город, в котором живет этот человек.
 # Словарь должен содержать ключи с такими именами, как first_name, last_name, age и city.
 # Выведите каждый фрагмент информации, хранящийся в словаре.
+
+people = {'first_name': 'Sergey', 'last_name': 'Morkovkin',
+          'age': 27, 'city': 'Volgograd'}
+
+print(people.get('first_name', 'sorry'))
+print(people.get('last_name', 'sorry'))
+print(people.get('age', 'sorry'))
+print(people.get('city', 'sorry'))
