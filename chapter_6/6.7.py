8f90072576fea7d593905170865396d07800217d
serejapwnz <oofatherxomgoo@gmail.com>
2023-10-09 18:56:14 +0300

    add 6.7

---
+++ chapter_6/6.7.py
@@ -0,0 +1,13 @@
+# Люди: начните с программы, написанной для упражнения 6.1 (с. 113). Создайте два новых словаря, представляющих разных людей, и сохраните все три словаря в списке с именем people. Переберите элементы списка людей.
+
+people = {'first_name': 'Sergey', 'last_name': 'Morkovkin',
+          'age': 27, 'city': 'Volgograd'}
+
+people_0 = {'first_name': 'Sergey', 'last_name': 'Morkovkin',
+            'age': 27, 'city': 'Volgograd'}
+
+people_1 = {'first_name': 'Sergey', 'last_name': 'Morkovkin',
+            'age': 27, 'city': 'Volgograd'}
+
+people_up = [people_1, people, people_0]
+print(people_up)
