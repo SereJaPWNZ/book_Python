3fcb7fc3f6a9217b0040d0cc0df467f82d09f26e
serejapwnz <oofatherxomgoo@gmail.com>
2023-10-16 22:56:59 +0300

    add 8.2

---
+++ chapter_8/8.2.py
@@ -0,0 +1,8 @@
+# Любимая книга: напишите функцию favorite_book(), которая получает один параметр title. Функция должна выводить сообщение вида «One of my favorite books is Alice in Wonderland». Вызовите функцию и убедитесь в том, что название книги правильно передается как аргумент при вызове функции.
+
+def favorite_book(title):
+    '''Вывод сообщения'''
+    print(f'One of my favorite books is {title}')
+
+
+favorite_book('Alice in Wonderland')
