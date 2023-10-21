2e375425a6817f6febba166546d3d667d1837c00
serejapwnz <oofatherxomgoo@gmail.com>
2023-10-21 20:03:00 +0300

    удалил вывод сообщения в функции show_messages

--- chapter_8/8.11.py
+++ chapter_8/8.11.py
@@ -1,12 +1,9 @@
 # Архивированные сообщения: начните с программы из упражнения 8.10. Вызовите функцию send_messages() для копии списка сообщений. После вызова функции выведите оба списка и убедитесь в том, что в исходном списке остались все сообщения.
-
-
 def sent_messages(messages, complited_sents):
     '''Отправка сообщения и добавление его в список sent_messages'''
     while messages:
         sent_message = messages.pop()
         complited_sents.append(sent_message)
-        print('\nСообщение отправлено')
 
 
 def show_messages(complited_sents):
