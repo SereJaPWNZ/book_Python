7514d871989b7de3b8616ea599546c8276ac7186
serejapwnz <oofatherxomgoo@gmail.com>
2023-10-22 22:08:52 +0300

    add make_shirt

--- chapter_8/8.16/8.16.py
+++ chapter_8/8.16/8.16.py
@@ -1,7 +1,19 @@
 # Импортирование: возьмите за основу одну из написанных вами программ с одной функцией. Сохраните эту функцию в отдельном файле. Импортируйте функцию в файл основной программы и вызовите функцию каждым из следующих способов:
 
+# import make_shirt
+# from make_shirt import make_shirt
+# from make_shirt import make_shirt as ms
+# import make_shirt as ms1
+from make_shirt import *
+
 # import имя_модуля
 # from имя_модуля import имя_функции
 # from имя_модуля import имя_функции as псевдоним
 # import имя_модуля as псевдоним
 # from имя_модуля import *
+
+# make_shirt.make_shirt()
+# make_shirt()
+# ms()
+# ms1.make_shirt()
+make_shirt()
---
+++ chapter_8/8.16/make_shirt.py
@@ -0,0 +1,2 @@
+def make_shirt(tshort='I love Python', size='L'):
+    print(f'Футболка с текстом: "{tshort}", размер: {size}')
