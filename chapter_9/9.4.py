4f3879a3ef12c1ad84137cd59ddbb8842bc62047
Sergey Morkovkin <oofatherxomgoo@gmail.com>
2023-10-29 09:33:56 +0000

    исправлена оригинальная задача

--- chapter_9/9.4.py
+++ chapter_9/9.4.py
@@ -5,37 +5,32 @@ class Restaurant():
 
     def __init__(self, restaurant_name, cuisine_type):
         '''Инициация атрибутов названия ресторана и его типа кухни'''
-        self.restaurant_name = restaurant_name  # инициализация имени ресторана
-        self.cuisine_type = cuisine_type  # инициализация типа кухни
-        self.number_served = 0  # инициализация количества обслуженных посетителей
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0  # количество обслуженных посетителей
 
     def describe_restaurant(self):
         '''Вывод описания ресторана'''
-        print(f'Тип кухни - {self.cuisine_type}.')
+        print(f'Ресторан - {self.restaurant_name} c типом кухни - {self.cuisine_type}.')
 
     def open_restaurant(self):
-        '''Вывод названия ресторана'''
+        '''Вывод сообщения об открытии ресторана'''
         print(f'{self.restaurant_name} - ресторан открыт!')
 
-    def read_number_served(self):
-        '''Вывод количества обслуживаемых клиентов'''
+    def set_number_served(self, new_number_served):
+        '''Выставление количества обслуженных посетителей'''
+        self.number_served = new_number_served
         return self.number_served
     
-    def set_number_served(self, number_visitors):
-        '''Выставление количества обслуженных посетителей'''
-        self.number_served = number_visitors
-        return self.number_served
-
-    def increment_number_served(self, increment_number):
-        '''Увеличивание количество обслуженных посетителей на заданную величину.'''
-        self.number_served += increment_number
-        return self.number_served
+    def increment_number_served(self, increase_number):
+        '''Увеличиваем количество обслуженных посетителей на заданную величину'''
+        self.number_served += increase_number
 
 restaurant = Restaurant('Volgograd', 'Russian')
-print(restaurant.read_number_served())
-restaurant.number_served = 10
-print(restaurant.read_number_served())
+print(restaurant.number_served)
+restaurant.number_served = 15
+print(restaurant.number_served)
 restaurant.set_number_served(30)
-print(restaurant.read_number_served())
-restaurant.increment_number_served(50)
-print(restaurant.read_number_served())
+print(restaurant.number_served)
+restaurant.increment_number_served(30)
+print(restaurant.number_served)
