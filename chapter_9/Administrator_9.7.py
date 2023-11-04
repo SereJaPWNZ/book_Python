e6338c87591b1ea2c9b0815e2f217567e7abb59f
Sergey Morkovkin <oofatherxomgoo@gmail.com>
2023-11-04 09:47:23 +0000

    add argum privileges

--- chapter_9/Administrator_9.7.py
+++ chapter_9/Administrator_9.7.py
@@ -29,9 +29,10 @@ class User():
 class Admin(User):
     '''Учетный профиль администратора'''
 
-    def __init__(self, first_name, last_name, birthday, have_pets, privileges=['разрешено добавлять сообщения', 'разрешено удалять пользователей', 'разрешено банить пользователей']):
+    def __init__(self, first_name, last_name, birthday, have_pets, privileges=None):
         '''Инициализируем атрибуты родителя. Добавляем атрибут привилегий'''
         super().__init__(first_name, last_name, birthday, have_pets)
+        privileges = ['разрешено добавлять сообщения', 'разрешено удалять пользователей', 'разрешено банить пользователей']
         self.privileges = privileges
 
     def show_privileges(self):
