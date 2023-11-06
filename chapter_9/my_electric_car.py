43877b679a27a528c8ccfdb2356293bc644cd942
Sergey Morkovkin <oofatherxomgoo@gmail.com>
2023-11-06 12:23:33 +0000

    Добавление строки описания для импорта

--- chapter_9/my_electric_car.py
+++ chapter_9/my_electric_car.py
@@ -1,6 +1,7 @@
-from car import ElectricCar
+'''Набор классов для представления электромобилей.'''
+from electric_car import ElectricCar
 
 my_new_tesla = ElectricCar('audi', 'e-trone', 2024)
 print(my_new_tesla.get_descriptive_name())
 my_new_tesla.battery.describe_battery()
-my_new_tesla.battery.get_range()
+my_new_tesla.battery.get_range_iterable()
