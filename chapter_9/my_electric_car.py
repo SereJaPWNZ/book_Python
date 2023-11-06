5acc843ac76ab187b55c81dd82feefd3ab663a8a
Sergey Morkovkin <oofatherxomgoo@gmail.com>
2023-11-06 12:25:43 +0000

    использован псевдоним

--- chapter_9/my_electric_car.py
+++ chapter_9/my_electric_car.py
@@ -1,7 +1,7 @@
 '''Набор классов для представления электромобилей.'''
-from electric_car import ElectricCar
+from electric_car import ElectricCar as EC
 
-my_new_tesla = ElectricCar('audi', 'e-trone', 2024)
+my_new_tesla = EC('audi', 'e-trone', 2024)
 print(my_new_tesla.get_descriptive_name())
 my_new_tesla.battery.describe_battery()
 my_new_tesla.battery.get_range_iterable()
